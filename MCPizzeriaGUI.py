37efaa57049375d7353147baf1e9c2545585aae5
shinyluckpluh <dumpie.ed.beer@gmail.com>
2026-04-15 20:30:01 +0200

    4

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -24,7 +24,10 @@ venster.wm_title("MC Pizzeria")
 
 
 
-
+knopSluit = Button(venster, text="sluiten", width=12, command=venster.destroy)
+knopSluit.grid(row=17, column=4)
+labelIntro = Label(venster, text="Welkom!")
+labelIntro.grid(row=0, column=0, sticky="W")
 
 
 
